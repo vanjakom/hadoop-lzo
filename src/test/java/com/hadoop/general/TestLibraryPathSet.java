5238dce4a710bc8d581c8ae6bb8029218c0c8ea7
Vanja Komadinovic <vanjakom@gmail.com>
2013-01-26 14:18:30 +0100

    * added test to verify if java.library.path is set

---
+++ src/test/java/com/hadoop/general/TestLibraryPathSet.java
@@ -0,0 +1,21 @@
+package com.hadoop.general;
+
+import junit.framework.TestCase;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vanja
+ * Date: 1/26/13
+ * Time: 14:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestLibraryPathSet extends TestCase {
+    public void testLibraryPathSet() throws Exception {
+        String libraryPath = System.getProperty("java.library.path");
+
+        System.out.println("Library path: " + libraryPath);
+
+        assertNotNull(libraryPath);
+        assertEquals(libraryPath, "");
+    }
+}
